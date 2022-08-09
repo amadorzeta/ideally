b796654796e2970fe4c2724247430764fde8944c
amador <joseamadorzeta@gmail.com>
2022-08-09 08:36:18 -0500

    Create task endpoint added

--- src/server/router/tasks.ts
+++ src/server/router/tasks.ts
@@ -2,8 +2,21 @@ import { createRouter } from "./context";
 import { prisma } from "../db/client";
 import { z } from "zod";
 
-export const taskRouter = createRouter().query("get-all", {
-  async resolve() {
-    return await prisma.task.findMany();
-  },
-});
+export const taskRouter = createRouter()
+  .query("get-all", {
+    async resolve() {
+      return await prisma.task.findMany();
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string().min(5).max(600),
+    }),
+    async resolve({ input }) {
+      return await prisma.task.create({
+        data: {
+          name: input.name,
+        },
+      });
+    },
+  });
